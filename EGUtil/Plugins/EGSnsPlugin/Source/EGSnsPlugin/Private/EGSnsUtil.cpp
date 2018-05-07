89ce923de617b93c196b07b328759b52d5c9e01d
lsm1987 <1987lsm@gmail.com>
2018-05-07 19:43:25 +0900

    SNS 서비스 초기화, 정리 로직 추가

--- EGUtil/Plugins/EGSnsPlugin/Source/EGSnsPlugin/Private/EGSnsUtil.cpp
+++ EGUtil/Plugins/EGSnsPlugin/Source/EGSnsPlugin/Private/EGSnsUtil.cpp
@@ -9,10 +9,65 @@ void FEGSnsUtil::Initialize()
 {
 	UE_LOG(EGSnsLog, Log, TEXT("FEGSnsUtil::Initialize()"));
 
-	if (GetDefault<UEGSnsPluginRuntimeSettings>()->IsTwitterEnable())
+	InitializeService(EEGSnsServiceType::Twitter);
+}
+
+void FEGSnsUtil::Finalize()
+{
+	UE_LOG(EGSnsLog, Log, TEXT("FEGSnsUtil::Finalize()"));
+
+	FinalizeService(EEGSnsServiceType::Twitter);
+}
+
+TSharedPtr<IEGSnsService> FEGSnsUtil::FindService(EEGSnsServiceType ServiceType)
+{
+	TSharedPtr<IEGSnsService>* Service = Services.Find(ServiceType);
+	return (Service != nullptr) ? *Service : nullptr;
+}
+
+bool FEGSnsUtil::IsServiceEnabled(EEGSnsServiceType ServiceType)
+{
+	switch (ServiceType)
 	{
-		TSharedPtr<FEGSnsTwitter> Service(new FEGSnsTwitter());
-		Service->Initialize();
-		Services.Add(EEGSnsServiceType::Twitter, Service);
+	case EEGSnsServiceType::Twitter:
+		return GetDefault<UEGSnsPluginRuntimeSettings>()->IsTwitterEnable();
+	default:
+		UE_LOG(EGSnsLog, Error, TEXT("Cannot find service enable setting. ServiceType: %d"), (int32)ServiceType);
+		return false;
 	}
+}
+
+void FEGSnsUtil::InitializeService(EEGSnsServiceType ServiceType)
+{
+	if (!IsServiceEnabled(ServiceType))
+	{
+		return;
+	}
+
+	TSharedPtr<IEGSnsService> Service;
+
+	switch (ServiceType)
+	{
+	case EEGSnsServiceType::Twitter:
+		Service = TSharedPtr<IEGSnsService>(new FEGSnsTwitter());
+	}
+
+	if (!Service.IsValid())
+	{
+		UE_LOG(EGSnsLog, Error, TEXT("Cannot create service. ServiceType: %d"), (int32)ServiceType);
+		return;
+	}
+
+	Service->Initialize();
+	Services.Add(ServiceType, Service);
+}
+
+void FEGSnsUtil::FinalizeService(EEGSnsServiceType ServiceType)
+{
+	TSharedPtr<IEGSnsService> Service = FindService(ServiceType);
+	if (Service.IsValid())
+	{
+		Service->Finalize();
+	}
+	Services.Remove(ServiceType);
 }
--- EGUtil/Plugins/EGSnsPlugin/Source/EGSnsPlugin/Public/EGSnsUtil.h
+++ EGUtil/Plugins/EGSnsPlugin/Source/EGSnsPlugin/Public/EGSnsUtil.h
@@ -13,7 +13,13 @@ class EGSNSPLUGIN_API FEGSnsUtil
 {
 public:
 	static void Initialize();
+	static void Finalize();
 
 private:
 	static TMap<EEGSnsServiceType, TSharedPtr<IEGSnsService>> Services;
+
+	static TSharedPtr<IEGSnsService> FindService(EEGSnsServiceType ServiceType);
+	static bool IsServiceEnabled(EEGSnsServiceType ServiceType);
+	static void InitializeService(EEGSnsServiceType ServiceType);
+	static void FinalizeService(EEGSnsServiceType ServiceType);
 };
