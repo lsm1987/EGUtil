29568695a1d8de683ae692268a0fe5e533f66327
lsm1987 <1987lsm@gmail.com>
2018-05-19 23:49:03 +0900

    테스트 프로젝트로부터 플러그인으로 자바 소스 복사해오는 배치파일 추가

---
+++ EGUtil/Plugins/EGSnsPlugin/CopySrcFromTestProject.bat
@@ -0,0 +1,2 @@
+rmdir /s /q "Build/Android/src/com/lsm1987/egsnsplugin"
+xcopy /s "..\..\..\TestProjects\EGSnsPlugin\app\src\main\java\com\lsm1987\egsnsplugin" "Build\Android\src\com\lsm1987\egsnsplugin\"
