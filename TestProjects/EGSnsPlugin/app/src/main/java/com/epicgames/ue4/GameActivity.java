e83c936e7ccefa16db42979364c7c33439425839
lsm1987 <1987lsm@gmail.com>
2018-05-22 14:38:14 +0900

    테스트용 프로젝트에 스크린샷 저장 기능 추가

--- TestProjects/EGSnsPlugin/app/src/main/java/com/epicgames/ue4/GameActivity.java
+++ TestProjects/EGSnsPlugin/app/src/main/java/com/epicgames/ue4/GameActivity.java
@@ -2,6 +2,8 @@ package com.epicgames.ue4;
 
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +13,9 @@ import com.lsm1987.egsnsplugin.EGSnsGameActivityUtil;
 import com.lsm1987.egsnsplugin.EGSnsTwitter;
 import com.lsm1987.egsnsplugin.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -41,6 +46,34 @@ public class GameActivity extends AppCompatActivity {
         EGSnsGameActivityUtil.onActivityResult(requestCode, resultCode, data);
     }
 
+    public void onClickBtnSaveScreenshot(View v) {
+        try {
+            File folder = new File(getScreenshotSaveDirPath());
+            folder.mkdirs();  //create directory
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            String filePath = getScreenshotSaveFilePath();
+            File imageFile = new File(filePath);
+            imageFile.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+
+            bitmap.compress(Bitmap.CompressFormat.PNG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            Log.d("GameActivity", "Screenshot saved. path:" + filePath);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+    }
+
     public void onClickBtnLogin(View v) {
         Log.d("GameActivity", "onClickBtnLogin");
         twitter.AndroidThunkJava_Login();
@@ -62,5 +95,15 @@ public class GameActivity extends AppCompatActivity {
         }
 
         return properties;
+    }
+
+    private String getScreenshotSaveDirPath()
+    {
+        return getExternalFilesDir(null).getPath() + File.separator + "Sns";
+    }
+
+    private String getScreenshotSaveFilePath()
+    {
+        return getScreenshotSaveDirPath() + File.separator + "Screenshot.png";
     }
 }
